fb41cc79ad5e38aac1ff1109a229871450a6e30c
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-14 20:41:55 +0100

    add bold style to the brand component

--- client/src/components/UI/Brand.tsx
+++ client/src/components/UI/Brand.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
+import '../../index.css';
 import styles from './Brand.module.css';
 
 const Brand: FC<{}> = () => {
   return (
-    <Link to='/' className={styles.brand}>
+    <Link to='/' className={`${styles.brand} bold`}>
       conduit
     </Link>
   );
