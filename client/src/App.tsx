0b26f030aa6d8efa24262ec5075a46e387b2db70
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-14 12:01:11 +0100

    add routing to the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,27 @@
-const App = () => {
-  return <div></div>;
+import { FC, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+
+const Home = lazy(() => {
+  return import('./pages/Home');
+});
+
+const PageNotFound = lazy(() => {
+  return import('./pages/PageNotFound');
+});
+
+const App: FC<{}> = () => {
+  const routes = (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  );
+
+  return <>{routes}</>;
 };
 
 export default App;
