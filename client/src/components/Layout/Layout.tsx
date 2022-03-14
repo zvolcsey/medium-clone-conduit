afdfed16b819ffbe1990b169c052e42ea678e166
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-14 11:59:09 +0100

    add layout component

---
+++ client/src/components/Layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { FC, Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import styles from './Layout.module.css';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout: FC<{}> = () => {
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <Suspense fallback='Loading...'>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
