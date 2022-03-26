734a46cef820afc56b105e1661736e55db40772f
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-26 23:08:53 +0100

    add className props

--- client/src/components/Tags/TagList.tsx
+++ client/src/components/Tags/TagList.tsx
@@ -3,10 +3,14 @@ import { FC } from 'react';
 import TagItem from './TagItem';
 import styles from './TagList.module.css';
 
-const TagList: FC<{ tags: string[] }> = (props) => {
+const TagList: FC<{ tags: string[]; className?: string }> = (props) => {
   const tagItems = props.tags.map((tag) => <TagItem key={tag} tag={tag} />);
 
-  return <ul className={styles['tag-list']}>{tagItems}</ul>;
+  return (
+    <ul className={`${styles['tag-list']} ${props.className ?? ''}`}>
+      {tagItems}
+    </ul>
+  );
 };
 
 export default TagList;
