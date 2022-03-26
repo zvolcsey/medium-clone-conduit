3ecf1976cd1d54228ccf83b545ff9972e565f467
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-26 23:04:15 +0100

    add button component

---
+++ client/src/components/UI/Button.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { ButtonType } from '../../app/types/components.types';
+
+import styles from './Button.module.css';
+
+const Button: FC<{
+  type?: ButtonType;
+  className?: string;
+  onClick: () => void;
+  disabled?: boolean;
+}> = (props) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${styles.button} ${props.className || ''}`}
+      onClick={props.onClick}
+      disabled={props.disabled || false}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
