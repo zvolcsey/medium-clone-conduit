e09886333fd3ccf727da845d497978e360cd4ab2
Zoltán Völcsey <zvolcsey@gmail.com>
2022-04-04 15:16:03 +0200

    working on the pageNotFound component

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../index.css';
 import styles from './PageNotFound.module.css';
 
 const PageNotFound: FC<{}> = () => {
   return (
-    <div className={`${styles['page-not-found']} centered bold`}>
-      404 Page Not Found!
-    </div>
+    <section className={`${styles['page-not-found']} centered`}>
+      <h1 className={`${styles['page-not-found__title']}`}>
+        404 Page Not Found!
+      </h1>
+      <Link to='/'>Back to Home</Link>
+    </section>
   );
 };
 
