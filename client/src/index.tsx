b0ff7032c13003074766bea71694c1786bcd6f03
Zoltán Völcsey <zvolcsey@gmail.com>
2022-04-11 11:40:51 +0200

    update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,11 +9,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
